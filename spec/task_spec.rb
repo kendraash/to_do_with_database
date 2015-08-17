e550b1c53018b8f39fb526d80277293a76a0cb32
Kendra Ash & Jing Wait <student@epicodus.com>
2015-08-17 10:28:16 -0700

    add Gemfile

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,13 +2,14 @@ require('rspec')
 require('pg')
 require('task')
 
-DB = PG.connect({dvname => 'to_do_test'})
+DB = PG.connect({:dbname => "to_do_test"})
 
 RSpec.configure do |config|
     config.after(:each) do
         DB.exec("DELETE FROM tasks *;")
     end
 end
+
 
 describe(Task) do
     describe('#==') do
